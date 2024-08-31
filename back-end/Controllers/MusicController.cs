6420c9c09205201159f0f25bba4e9f454a4791b5
francobottini99 <bottinifranco99@gmail.com>
2024-08-30 23:43:20 -0300

    My super first endpoint. Yupi !

---
+++ back-end/Controllers/MusicController.cs
@@ -0,0 +1,37 @@
+﻿using chopify.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace chopify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : Controller
+    {
+        private readonly IMongoDatabase _database;
+
+        public MusicController(IMongoDatabase database)
+        {
+            _database = database;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<ActionResult> Get()
+        {
+            var collection = _database.GetCollection<Music>("music");
+
+            var musicList = await collection.Find(_ => true)
+                                            .Project(m => new
+                                            {
+                                                id = m.InternalId,
+                                                m.Name,
+                                                m.Duration
+                                            })
+                                            .ToListAsync();
+
+            return Ok(musicList);
+        }
+    }
+}
