f624d2d27106b30fd0a5a58ccde7152a550f117d
Fausto Lavezzari <faustolavezzari99@gmail.com>
2024-08-31 00:03:00 -0300

    Back develop (#2)
    * My super first endpoint. Yupi !
    
    * First post endpoint
    
    * Add docker file !
    
    * First post endpoint
    
    * Add docker file !
    
    ---------
    
    Co-authored-by: francobottini99 <bottinifranco99@gmail.com>

---
+++ back-end/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using chopify.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace chopify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IMongoDatabase _database;
+
+        public UserController(IMongoDatabase database)
+        {
+            _database = database;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<ActionResult> Create([FromBody] UsernameRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username))
+            {
+                return BadRequest("Username is required.");
+            }
+
+            var user = new User
+            {
+                Username = request.Username,
+                Tag = GenerateShortTag()  // Generar el UUID truncado para el campo Tag
+            };
+
+            var collection = _database.GetCollection<User>("users");
+
+            try
+            {
+                await collection.InsertOneAsync(user);
+                return CreatedAtAction(nameof(Create), new {user.Username, user.Tag});
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private string GenerateShortTag()
+        {
+            // Generar un UUID y truncarlo a 8 caracteres
+            return Guid.NewGuid().ToString("N").Substring(0, 8).ToUpper();
+        }
+    }
+
+    public class UsernameRequest
+    {
+        public string Username { get; set; } = null!;
+    }
+}
