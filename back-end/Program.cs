d8b1af84bdfd4a697cadf2f643395d049cb1b924
francobottini99 <bottinifranco99@gmail.com>
2024-09-20 10:30:04 -0300

    fix: :bug: disable https redirect

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseCors("AllowAllOrigins");
 //app.UseAuthentication();
 app.UseAuthorization();
